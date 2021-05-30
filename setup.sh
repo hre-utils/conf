44575796a2513858c13e009da6dbd15aa27d9fc4
Marcus Aurelius <marcus+git@aurelius.org>
2021-05-30 14:20:57 -0700

    Removed tmp files, created setup script.
    Core functionality done. Now a full replacement of the existing
    functionality from Python `conf`.
    
    Need to expand documentation.

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PROGDIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd )
+
+CONFDIR="${XDG_CONFIG_HOME:-$HOME/.config}/hre-utils/conf"
+mkdir -p "$CONFDIR"
+
+DATABASE="${CONFDIR}/database"
+touch "$DATABASE"
+
+CONFFILE="${CONFDIR}/config"
+cp -vn "${PROGDIR}/doc/config" "${CONFFILE}"
